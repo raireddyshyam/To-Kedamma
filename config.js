33be25039854d6dfd18f03ee1713d9b5354e81b9
raireddyshyam <raireddyshyam@gmail.com>
2024-06-22 10:22:32 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+
+var config = {
+    lines: [
+        "Dear Kedamma,",
+        "I never knew how much you mean to me when we first started talking. Until today,i still love you as much as I did since the very first day we met. ",
+        " Every since you came into my life, I felt like I was the happiest. I just want to let you know how much you mean to me and how much I appreciate you.",
+        "You never failed to make me Smile. Things aren't getting better every since we fought, I just miss you so much. Having random break downs, with you on my mind wondering If you're doing fine. Im sorry I wasn't what you wanted,",
+        "Im sorry I wasn't the best, Im sorry I couldn't be good enough for you.I just hope everything will be okay soon, I really miss how we used to talk Before. I cant leave you at any cost kopam lo matalu anastha anthey but nv antey naku pranam kannama. LOVE YOU SO MUCH BNGRM AND AM SORY......Kinda kanipinchey time na last breath tho agali anthey kani madhyalo agakudadhu.....", 
+       
+    ],
+    date:"2024-05-01T20:15:00",
+    names: ["☃︎【 Our Relationship Time 】","‎ "]
+}
